2b18e2d02182ddd4283ae21861340144ca9f7276
Anmol (WSL) <anmol.srivastava.ca@gmail.com>
2020-05-30 00:07:09 -0400

    oof

--- voter_suppression_analysis/data/processing.py
+++ voter_suppression_analysis/data/processing.py
@@ -6,11 +6,15 @@ import glob
 import pandas as pd
 
 
+#############
+# CONSTANTS #
+#############
+
+
 # useful constants for accessing files
 AGE_PATH_REGEX = 'clean/*_age.csv'
 SEX_PATH_REGEX = 'clean/*_sexrace.csv'
 LAWS_DATA_PATH = 'clean/suppression.csv'
-
 
 # useful constants for renaming and removing columns
 AGE_COLUMN_NAMES = [
@@ -18,15 +22,44 @@ AGE_COLUMN_NAMES = [
     'ci_reg', 'total_voted', 'percent_voted', 'ci_voted', 'yr'
 ]
 
-
 SEX_COLUMN_NAMES = [
     'state', 'group', 'pop', 'total_cit', 'percent_cit', 'ci_cit',
     'total_reg', 'percent_reg', 'ci_reg', 'total_voted', 'percent_voted',
     'ci_voted', 'yr'
 ]
 
-KEEP_AGE_COLUMNS = ['state', 'age_bracket', 'total', 'total_reg', 'total_voted', 'percent_reg', 'percent_voted', 'yr']
+KEEP_AGE_COLUMNS = [
+    'state', 'age_bracket', 'total', 'total_reg', 'total_voted', 
+    'percent_reg', 'percent_voted', 'yr'
+]
+
 KEEP_SEX_COLUMNS = ['state', 'group', 'percent_reg', 'percent_voted', 'yr']
+
+# useful constants for standardizing state labels 
+STATE_NAMES = ['ALABAMA', 'ALASKA', 'ARIZONA', 'ARKANSAS', 'CALIFORNIA', 
+    'COLORADO', 'CONNECTICUT', 'DELAWARE', 'DISTRICT OF COLUMBIA', 'FLORIDA', 
+    'GEORGIA', 'HAWAII', 'IDAHO', 'ILLINOIS', 'INDIANA', 'IOWA', 'KANSAS', 
+    'KENTUCKY', 'LOUISIANA', 'MAINE', 'MARYLAND', 'MASSACHUSETTS', 
+    'MICHIGAN', 'MINNESOTA', 'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 
+    'NEVADA', 'NEW HAMPSHIRE', 'NEW JERSEY', 'NEW MEXICO', 'NEW YORK',
+    'NORTH CAROLINA', 'NORTH DAKOTA', 'OHIO', 'OKLAHOMA', 'OREGON',
+    'PENNSYLVANIA', 'RHODE ISLAND', 'SOUTH CAROLINA', 'SOUTH DAKOTA',
+    'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGINIA', 'WASHINGTON',
+    'WEST VIRGINIA', 'WISCONSIN', 'WYOMING'
+]
+
+# note these integers are related to US Census Bureau ordering 
+STATE_NUMS = [1,  2,  4,  5,  6,  8,  9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
+    38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56,
+]
+
+STATES_TABLE = [STATE_NAMES, STATE_NUMS]
+
+
+#############
+# FUNCTIONS #
+#############
 
 
 def get_age_df(file_path):
@@ -110,9 +143,10 @@ def combine_age_data():
                                how='outer',
                                left_on='state',
                                right_on='state')
+    
+    # make nationwide labels consistent and finish 
     result_df.state.loc[result_df.state == 'US'] = 'NATIONAL'
     result_df.state.loc[result_df.state == 'UNITED STATES'] = 'NATIONAL'
-
     return result_df
 
 
@@ -146,11 +180,14 @@ def combine_sexrace_data():
                                how='outer',
                                left_on='state',
                                right_on='state')
+    
+    # make nationwide labels consistent and finish 
     result_df.state.loc[result_df.state == 'US'] = 'NATIONAL'
     result_df.state.loc[result_df.state == 'UNITED STATES'] = 'NATIONAL'
     return result_df
 
-def setup_age_dataframe(df_in):
+
+def homogenize_age_data(df):
     """
     Structures the age data by creating the desired age groups
     of 'Total','18 to 44', '45 to 65', '65+' into a DataFrame.
@@ -161,56 +198,60 @@ def setup_age_dataframe(df_in):
     Returns:
         pd.DataFrame, age bracket structured data for all years
     """
+    df_states = pd.DataFrame(STATES_TABLE, columns=['state', 'id'])
+    df_states.state = df_states.state.str.capitalize()
 
-    # Setting ID numbers to match US Census for each state
-    STATEID=[['ALABAMA', 'ALASKA', 'ARIZONA', 'ARKANSAS',
-        'CALIFORNIA', 'COLORADO', 'CONNECTICUT', 'DELAWARE',
-        'DISTRICT OF COLUMBIA', 'FLORIDA', 'GEORGIA', 'HAWAII', 'IDAHO',
-        'ILLINOIS', 'INDIANA', 'IOWA', 'KANSAS', 'KENTUCKY', 'LOUISIANA',
-        'MAINE', 'MARYLAND', 'MASSACHUSETTS', 'MICHIGAN', 'MINNESOTA',
-        'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 'NEVADA',
-        'NEW HAMPSHIRE', 'NEW JERSEY', 'NEW MEXICO', 'NEW YORK',
-        'NORTH CAROLINA', 'NORTH DAKOTA', 'OHIO', 'OKLAHOMA', 'OREGON',
-        'PENNSYLVANIA', 'RHODE ISLAND', 'SOUTH CAROLINA', 'SOUTH DAKOTA',
-        'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGINIA', 'WASHINGTON',
-        'WEST VIRGINIA', 'WISCONSIN', 'WYOMING'],
-             [1,  2,  4,  5,  6,  8,  9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20,
-              21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-              38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49, 50, 51, 53, 54, 55, 56,]]
-    idState = pd.DataFrame(STATEID).T
-    idState.columns = ['state','id']
-    idState.state = idState.state.str.capitalize()
+    # splitting inconsistent age brackets into many DFs
+    df_65plus = df.loc[(df_in.age_bracket == '65 to 74') 
+        | (df_in.age_bracket == '75+') 
+        | (df_in.age_bracket == '65 to 75') 
+        | (df_in.age_bracket == '65+'),
+    ]
 
-    # creating individual age bracket dataframes
-    df65plus = df_in.loc[(df_in.age_bracket == '65 to 74') | (df_in.age_bracket == '75+') |
-                         (df_in.age_bracket == '65 to 75') | (df_in.age_bracket == '65+'),]
-    df4564plus = df_in.loc[(df_in.age_bracket == '45 to 64') | (df_in.age_bracket == '45 to 55') |
-                           (df_in.age_bracket == '55 to 65') | (df_in.age_bracket == '45 to 65'),]
-    df1844plus = df_in.loc[(df_in.age_bracket == '18 to 24') | (df_in.age_bracket == '18 to 25')| 
-                           (df_in.age_bracket == '25 to 44') | (df_in.age_bracket == '25 to 35') | 
-                           (df_in.age_bracket == '35 to 45') | (df_in.age_bracket == '25 to 45') |
-                           (df_in.age_bracket == '25 to 34') | (df_in.age_bracket == '35 to 44'),]
-    dftotal = df_in.loc[df_in.age_bracket == 'Total',]
+    df_45_64 = df.loc[(df_in.age_bracket == '45 to 64') 
+        | (df_in.age_bracket == '45 to 55') 
+        | (df_in.age_bracket == '55 to 65') 
+        | (df_in.age_bracket == '45 to 65'),
+    ]
 
-    # setting values and parameters to be fed into for loop
-    dfs_age = [dftotal, df1844plus, df4564plus, df65plus]
-    age_bracket = ['Total','18 to 44', '45 to 65', '65+']
-    dfages = []
-    for i, df in enumerate(dfs_age):
+    df_18_44 = df.loc[(df_in.age_bracket == '18 to 24') 
+        | (df_in.age_bracket == '18 to 25')
+        | (df_in.age_bracket == '25 to 44') 
+        | (df_in.age_bracket == '25 to 35') 
+        | (df_in.age_bracket == '35 to 45') 
+        | (df_in.age_bracket == '25 to 45') 
+        | (df_in.age_bracket == '25 to 34') 
+        | (df_in.age_bracket == '35 to 44'),
+    ]
+
+    df_total = df.loc[df.age_bracket == 'Total',]
+
+    # iteratively group these DFs by state and year 
+    df_list = [df_total, df_18_44, df_45_64, df_65plus]
+    age_brackets = ['Total','18 to 44', '45 to 65', '65+']
+    result = []
+
+    for i, df in enumerate(df_list):
         df = df.groupby(['state', 'yr'], sort=False).sum().reset_index()
-        df['age_bracket'] = age_bracket[i]
-        dfages.append(df)
+        df['age_bracket'] = age_brackets[i]
+        result.append(df)
 
-    dfages = pd.concat(dfages, axis=0, ignore_index=True)
-    dfages.sort_values(['yr','state'], inplace=True)
+    # recombine all age bracket DFs
+    result = pd.concat(result, axis=0, ignore_index=True)
+    result.sort_values(['yr', 'state'], inplace=True)
 
-    # calculating the Percentage value of voter turnout
-    dfages['percent_reg'] = dfages.total_reg / dfages.total
-    dfages['percent_voted'] = dfages.total_voted / dfages.total
-    dfages.yr = dfages.yr.astype(int)
-    dfages.state = dfages.state.str.capitalize()
+    # compute voter turnout metric
+    result['percent_reg'] = result.total_reg / result.total
+    result['percent_voted'] = result.total_voted / result.total
 
-    # adding the ID number to the states
-    df_out = pd.merge(dfages, idState, left_on='state', right_on='state')
+    # refomatting
+    result.yr = result.yr.astype(int)
+    result.state = result.state.str.capitalize()
 
-    return df_out
+    # attach our state labels/IDs and finish 
+    result = pd.merge(result, df_states, left_on='state', right_on='state')
+    return result
+
+
+def homogenize_sexrace_data(df):
+    raise NotImplemented
