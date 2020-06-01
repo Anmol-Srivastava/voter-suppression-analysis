fd435f15130112a04a3c2f2bca866611f7eccc48
Anmol (WSL) <anmol.srivastava.ca@gmail.com>
2020-05-31 22:20:33 -0400

    pylint will be done manually

--- voter_suppression_analysis/tests/test_data.py
+++ voter_suppression_analysis/tests/test_data.py
@@ -1,2 +1,17 @@
-def test_data_1():
-    assert True 
+def test_get_age_df():
+    pass 
+
+def test_get_sexrace_df():
+    pass
+
+def test_combine_age_data():
+    pass
+
+def test_combine_sexrace_data():
+    pass
+
+def homogenize_age_data():
+    pass
+
+def homogenize_sexrace_data():
+    pass 
